b45be1e93ebcef023a98ab95b0a8a35d7b01d2b4
JordanKoeller <jkoeller12@gmail.com>
2018-11-26 00:30:55 -0600

    Wrote code for velocity, but haven't tested yet

--- mirage/calculator/MassFunction.py
+++ mirage/calculator/MassFunction.py
@@ -85,6 +85,83 @@ class StationaryMassFunction(MassFunction):
             return self._stars
 
 
+class AnimatedMassFunction(MassFunction):
+
+    def __init__(self,IMF:IMF_broken_powerlaw,velocity:u.Quantity,sigma:u.Quantity,dt:u.Quantity,seed:int=None) -> None:
+        self._stationaryFunc = StationaryMassFunction(IMF,seed)
+        self._velocity_characteristics = (velocity,sigma.to(velocity.unit))
+        self._region_info = None
+        self._time = dt*0.0
+        self._dt = dt
+        self._stars_start_pos = []
+        self._velocity = None
+
+    def generate_stars(self,region:Region,mass_density:u.Quantity,center_on_zero=False) -> np.ndarray:
+        self._region_info == (region,mass_density,center_on_zero)
+        self._stars_start_pos = self._stationaryFunc.generate_stars(region,mass_density,center_on_zero)
+        rng = self._stationaryFunc._IMF.random_number_generator
+        velocity, sigma = self._velocity_characteristics
+        velocity_mags = rng.normal(velocity.value,sigma.value,len(self._stars_start_pos))
+        velocity_directions = rng.rand(len(self._stars_start_pos),3)
+        velocity_3d = velocity_mags*velocity_directions/np.sqrt(velocity_directions[:,0]**2+velocity_directions[:,1]**2+velocity_directions[:,2]**2)
+        self._velocity = u.Quantity(velocity_3d[:,0:2],velocity.unit)
+        return self.stars
+        # I just remove the z direction, leaving just the two others after it all.
+
+
+    @property
+    def stars(self):
+        if len(self._stars_start_pos) > 0:
+            tmp_stars = self._stars_start_pos.copy()
+            tmp_stars[:,0] = tmp_stars[:,0] + (self._velocity[:,0]*self._time).value
+            tmp_stars[:,1] = tmp_stars[:,1] + (self._velocity[:,1]*self._time).value
+            return tmp_stars
+        else:
+            raise EnvironmentError("Never generated stars to begin with.")
+
+    def _make_initial_stars(self,region:Region,mass_density:u.Quantity,center_on_zero=False) -> None:
+        pass
+
+    def set_time(self,time:u.Quantity) -> None:
+        self._time = time
+
+    def increment_time(self) -> None:
+        self._time += self._dt
+
+    def skip_to_frame(self,num:int) -> None:
+        self._time = self._dt*num
+
+    @property
+    def json(self):
+        ret = self._stationaryFunc.json
+        ret['mean_velocity'] = Jsonable.encode_quantity(self._velocity_characteristics[0])
+        ret['stderr_velocity'] = Jsonable.encode_quantity(self._velocity_characteristics[1])
+        ret['dt'] = Jsonable.encode_quantity(self._dt)
+        return ret
+
+    @classmethod
+    def from_json(cls,js):
+        seed = js['seed']
+        ml = js['mass_limits']
+        pows = js['powers']
+        velocity = Jsonable.decode_quantity(js['mean_velocity'])
+        sigma = Jsonable.decode_quantity(js['stderr_velocity'])
+        dt = Jsonable.decode_quantity(js['dt'])
+        if 'conversions' in js:
+            convs = js['conversions']
+            imf = Evolved_IMF(conversions = convs,
+                powers = pows,
+                massLimits = ml)
+            return cls(imf,velocity,sigma,dt,seed)
+        else:
+            imf = IMF_broken_powerlaw(ml,pows)
+            return cls(imf,velocity,sigma,dt,seed)
+
+
+
+
+
+
 def getMassFunction() -> MassFunction:
     from .InitialMassFunction import Kroupa_2001, Kroupa_2001_Modified
     # from .InitialMassFunction import Kroupa_2001
@@ -101,7 +178,6 @@ def getMassFunction() -> MassFunction:
         return StationaryMassFunction(Kroupa_2001_Modified(),seed)
     elif fn == "Aged_galaxy":
         return StationaryMassFunction(Evolved_IMF(),seed)
-
     # #Means this is a custom IMF. It may or may not have aging thresholds.
     elif "mass_limits" in fn and "powers" in fn:
         imf = IMF_broken_powerlaw(np.array(fn['mass_limits']),np.array(fn['powers']))
@@ -112,5 +188,8 @@ def getMassFunction() -> MassFunction:
             return StationaryMassFunction(emf,seed)
     #     # ret = StationaryMassFunction(IMF_broken_powerlaw(np.array(fn['mass_limits']),np.array(fn['powers'])),seed)
     #     # if "conversions" in fn: ret = StationaryMassFunction(Evolved_IMF(ret,fn['conversions']),seed)
-    # else:
-    #     raise ValueError("Not a valid mass function. Please update your preferences.")
+    else:
+        raise ValueError("Not a valid mass function. Please update your preferences.")
+
+
+

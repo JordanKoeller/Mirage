89c6614b17611f64d6cc02963ebe533d8ce71813
JordanKoeller <jkoeller12@gmail.com>
2018-11-29 17:33:27 -0600

    Updates to the scripts directory

--- scripts/shell.py
+++ scripts/shell.py
@@ -1,7 +1,15 @@
 import os
 
-if "IS_SIMULATION" in os.environ:
+if "LA_IS_SIMULATION" in os.environ:
     from main import run_simulation
+
+if os.environ["LA_WITH_GUI"] == "yes":
+    try:
+        from matplotlib import pyplot as plt
+        from IPython import get_ipython
+        get_ipython().magic("matplotlib")
+    except:
+        print("Matplotlib imported. To run matplotlib interractively with ipython, run the command \n\n>>> %matplotlib")
 
 from mirage import lens_analysis as la
 from mirage.util import *
@@ -12,17 +20,6 @@ import mirage
 #Import general things
 from astropy import units as u
 import numpy as np
-from math import *
 import math
 
 # A few ipython things to manipulate the environment
-
-try:
-    from IPython import get_ipython
-    ipython = get_ipython()
-    ipython.magic('matplotlib')
-    from matplotlib import pyplot as plt
-    #cleanup
-    del(ipython)
-except ImportError:
-    print("Running without ipython")

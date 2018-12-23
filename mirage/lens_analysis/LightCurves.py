2c3d2facd63cd09441729999c348ede8d2df95bd
JordanKoeller <jkoeller12@gmail.com>
2018-12-23 10:28:26 -0600

    patch

--- mirage/lens_analysis/LightCurves.py
+++ mirage/lens_analysis/LightCurves.py
@@ -20,7 +20,7 @@ def get_analyzed_events(filename:str,base,min_sep_coeff,**event_finding_args):
     lc1 = data[base].lightcurves
     r_g = data.simulation.parameters.quasar.r_g
     peaks = map(lambda e: e.get_events(min_separation=min_sep_coeff*r_g,**event_finding_args),lc1)
-    for ind in range(len(lc1)):
+    for ind in range(int(len(lc1)/2)):
         peak_batch = next(peaks)
         for peak in peak_batch:
             symm = peak.symmetry(min_sep_coeff*r_g)
